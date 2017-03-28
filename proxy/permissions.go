6ff6c5b5eeb53c548deffdc0a7ab6f07fd9e9b57
Jake Moshenko <jake.moshenko@coreos.com>
2017-03-28 10:55:25 -0400

    Add the rest of the resources that I forgot the first time

--- proxy/permissions.go
+++ proxy/permissions.go
@@ -23,17 +23,48 @@ type StripeResource int
 // Note: these do not use iota so that they are stable through modifications
 // of the list.
 const (
-	ResourceAll               StripeResource = 0
-	ResourceBalance                          = 1
-	ResourceCharges                          = 2
-	ResourceCustomers                        = 3
-	ResourceDisputes                         = 4
-	ResourceEvents                           = 5
-	ResourceFileUploads                      = 6
-	ResourceRefunds                          = 7
-	ResourceTokens                           = 8
-	ResourceTransfers                        = 9
-	ResourceTransferReversals                = 10
+	ResourceAll StripeResource = 0
+
+	// Core resources
+	ResourceBalance           = 1
+	ResourceCharges           = 2
+	ResourceCustomers         = 3
+	ResourceDisputes          = 4
+	ResourceEvents            = 5
+	ResourceFileUploads       = 6
+	ResourceRefunds           = 7
+	ResourceTokens            = 8
+	ResourceTransfers         = 9
+	ResourceTransferReversals = 10
+
+	// Connect resources
+	ResourceAccount              = 11
+	ResourceApplicationFeeRefund = 12
+	ResourceApplicationFee       = 13
+	ResourceRecipient            = 14
+	ResourceCountrySpec          = 15
+	ResourceExternalAccount      = 16
+
+	// Payment methods
+	ResourceSource = 17
+
+	// Relay resources
+	ResourceOrder       = 18
+	ResourceOrderReturn = 19
+	ResourceProduct     = 20
+	ResourceSKU         = 21
+
+	// Subscription resources
+	ResourceCoupon           = 22
+	ResourceInvoice          = 23
+	ResourceInvoiceItem      = 24
+	ResourcePlan             = 25
+	ResourceSubscription     = 26
+	ResourceSubscriptionItem = 27
+
+	// Radar resources
+	ResourceRadarReview = 28
+	ResourceRadarRule   = 29
 )
 
 type Access int
@@ -48,36 +79,38 @@ const (
 )
 
 type Permission struct {
-	encoded uint32
+	encoded uint64
 }
 
-func NewPermission(initialValue uint32) *Permission {
+func NewPermission(initialValue uint64) *Permission {
 	return &Permission{initialValue}
 }
 
 func (p *Permission) MarshalBinary() ([]byte, error) {
-	bs := make([]byte, 4)
-	binary.BigEndian.PutUint32(bs, p.encoded)
+	bs := make([]byte, 8)
+	binary.BigEndian.PutUint64(bs, p.encoded)
 	return bs, nil
 }
 
 func (p *Permission) BinaryUnmarshaler(data []byte) error {
-	p.encoded = binary.BigEndian.Uint32(data)
+	p.encoded = binary.BigEndian.Uint64(data)
 	return nil
 }
 
-func resourceMask(resource StripeResource, access Access) uint32 {
-	return uint32(access << (uint32(resource) * 2))
+func resourceMask(access Access, resources ...StripeResource) uint64 {
+	var mask uint64
+	for _, resource := range resources {
+		mask |= uint64(access << (uint64(resource) * 2))
+	}
+	return mask
 }
 
-func (p *Permission) Can(access Access, resource StripeResource) bool {
-	mask := resourceMask(resource, access)
-	allMask := resourceMask(ResourceAll, access)
+func (p *Permission) Can(access Access, resources ...StripeResource) bool {
+	mask := resourceMask(access, resources...)
+	allMask := resourceMask(access, ResourceAll)
 	return mask&p.encoded == mask || allMask&p.encoded == allMask
 }
 
 func (p *Permission) SetAccess(access Access, resources ...StripeResource) {
-	for _, resource := range resources {
-		p.encoded |= resourceMask(resource, access)
-	}
+	p.encoded |= resourceMask(access, resources...)
 }
